31fc385cd1cf9938afcaaee8b9cadfd1d59c5963
Michelle Madeira <michellemadeira1@gmail.com>
2024-07-22 16:04:45 -0300

    Start-Project

---
+++ src/main/java/com/teratura_aromas/TeraturaAromasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teratura_aromas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeraturaAromasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeraturaAromasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teratura_aromas/TeraturaAromasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teratura_aromas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeraturaAromasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
